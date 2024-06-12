3582c3e37337a26cb36e891ac1002542d027b14c
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-06-12 00:00:41 -0500

    Update CombineHistograms.cpp

--- CombineHistograms.cpp
+++ CombineHistograms.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <map>
+#include <functional>
 #include <TFile.h>
-#include <TH1.h>
-#include <TCanvas.h>
 #include <TKey.h>
+#include <TH1.h>
 #include <TSystem.h>
-#include <TDirectory.h>
+#include <TList.h>
+#include <TString.h>
 
 void CombineHistograms(const char* directoryPath) {
     // Set the output file name
-    TString outputPath = TString(directoryPath) + "/HistCombined44686.root";
+    TString outputPath = TString(directoryPath) + "/combined/Combined_TriggerHists.root";
 
     // Open the output file
     TFile *outputFile = new TFile(outputPath, "RECREATE");
@@ -20,7 +22,9 @@ void CombineHistograms(const char* directoryPath) {
     // List all files in the specified directory ending with ".root"
     void *dirp = gSystem->OpenDirectory(directoryPath);
     const char *filename;
+
     std::map<std::string, TH1*> histogramMap;
+    std::map<std::string, TDirectory*> directoryMap;
 
     while ((filename = gSystem->GetDirEntry(dirp))) {
         TString filepath = TString(directoryPath) + "/" + TString(filename);
@@ -33,36 +37,70 @@ void CombineHistograms(const char* directoryPath) {
         }
 
         std::cout << "Processing file: " << filepath << std::endl;
-        TList *list = file->GetListOfKeys();
-        TIter iter(list);
-        TKey *key;
 
-        while ((key = (TKey*)iter())) {
-            TObject *obj = key->ReadObj();
-            if (obj->InheritsFrom("TH1")) {
-                TH1 *hist = (TH1*)obj;
-                std::cout << "Processing histogram: " << hist->GetName() << std::endl;
+        // Function to recursively process directories
+        std::function<void(TDirectory*, const std::string&)> processDirectory = [&](TDirectory* sourceDir, const std::string& parentPath) {
+            TList *list = sourceDir->GetListOfKeys();
+            TIter iter(list);
+            TKey *key;
 
-                // Only process histograms with entries
-                if (hist->GetEntries() > 0) {
-                    if (histogramMap.find(hist->GetName()) == histogramMap.end()) {
-                        histogramMap[hist->GetName()] = (TH1*)hist->Clone();
-                        histogramMap[hist->GetName()]->SetDirectory(0); // Remove from current file directory
-                    } else {
-                        histogramMap[hist->GetName()]->Add(hist);
+            while ((key = (TKey*)iter())) {
+                TObject *obj = key->ReadObj();
+                if (obj->InheritsFrom("TH1")) {
+                    TH1 *hist = (TH1*)obj;
+                    std::string histPath = parentPath + "/" + hist->GetName();
+                    std::cout << "Processing histogram: " << histPath << std::endl;
+
+                    if (hist->GetEntries() > 0) {
+                        if (histogramMap.find(histPath) == histogramMap.end()) {
+                            histogramMap[histPath] = (TH1*)hist->Clone();
+                            histogramMap[histPath]->SetDirectory(0); // Remove from current file directory
+                        } else {
+                            histogramMap[histPath]->Add(hist);
+                        }
                     }
+                } else if (obj->InheritsFrom("TDirectory")) {
+                    TDirectory *subDir = (TDirectory*)obj;
+                    std::string subDirPath = parentPath + "/" + subDir->GetName();
+                    if (directoryMap.find(subDirPath) == directoryMap.end()) {
+                        // Navigate to the parent directory in the output file
+                        TDirectory* parentDir = outputFile;
+                        if (!parentPath.empty()) {
+                            parentDir = directoryMap[parentPath];
+                            if (!parentDir) {
+                                std::cerr << "Parent directory not found for: " << parentPath << std::endl;
+                                continue;
+                            }
+                        }
+                        parentDir->cd();
+                        TDirectory *newDir = parentDir->mkdir(subDir->GetName());
+                        directoryMap[subDirPath] = newDir;
+                    }
+                    processDirectory(subDir, subDirPath);
                 }
             }
-        }
+        };
+
+        processDirectory(file, "");
+
         file->Close();
     }
 
     // Write combined histograms to the output file
-    outputFile->cd();
     for (auto &entry : histogramMap) {
+        size_t pos = entry.first.find_last_of('/');
+        std::string dirPath = entry.first.substr(0, pos);
+        std::string histName = entry.first.substr(pos + 1);
+
+        if (directoryMap.find(dirPath) != directoryMap.end()) {
+            directoryMap[dirPath]->cd();
+        } else {
+            outputFile->cd();
+        }
+
         if (entry.second->GetEntries() > 0) { // Ensure it has entries before saving
-            std::cout << "Writing histogram: " << entry.first << " with " << entry.second->GetEntries() << " entries." << std::endl;
-            entry.second->Write();
+            std::cout << "Writing histogram: " << histName << " with " << entry.second->GetEntries() << " entries." << std::endl;
+            entry.second->Write(histName.c_str());
         }
     }
 
