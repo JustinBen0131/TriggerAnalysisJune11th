a8f1e2fc2d400ad6df14582d4cbdf362d0f499b5
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-06-11 23:03:51 -0500

    Update triggerCondor.sh

--- condorScripts/triggerCondor.sh
+++ condorScripts/triggerCondor.sh
@@ -8,24 +8,27 @@ source /opt/sphenix/core/bin/sphenix_setup.sh -n
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 
 # Capture command line arguments
-runNumber=$1
+runNumbers=($1)
 filename=$2
 clusterID=$3
 events=0  # Default number of events
 
-# Create a directory based on the run number
-outputDir="output/${runNumber}"
-mkdir -p ${outputDir}  # Ensure the directory exists
+for runNumber in "${runNumbers[@]}"; do
+    # Create a directory based on the run number
+    outputDir="output/${runNumber}"
+    mkdir -p ${outputDir}  # Ensure the directory exists
 
-# Construct the output filenames within the new directory
-treeOutName="${outputDir}/CaloOutput_${filename%.*}.root"  # Strip the extension from the filename and append
-dstOutName="${outputDir}/DST_EMULATOR_${filename%.*}.root"
+    # Construct the output filenames within the new directory
+    treeOutName="${outputDir}/CaloOutput_${filename%.*}.root"  # Strip the extension from the filename and append
+    dstOutName="${outputDir}/DST_EMULATOR_${filename%.*}.root"
 
-# Check if filename is provided
-if [ -z "$filename" ]; then
-  echo "Error: Filename is not provided."
-  exit 1
-fi
+    # Check if filename is provided
+    if [ -z "$filename" ]; then
+        echo "Error: Filename is not provided."
+        exit 1
+    fi
 
-# Run the ROOT macro with dynamically generated output paths and log the output
-root -b -l -q "macros/Fun4All_Calo_Emulator.C(\"$filename\", $events, \"$treeOutName\", \"$dstOutName\")"
+    # Run the ROOT macro with dynamically generated output paths and log the output
+    root -b -l -q "macros/Fun4All_Calo_Emulator.C(\"$filename\", $events, \"$treeOutName\", \"$dstOutName\")"
+done
+
